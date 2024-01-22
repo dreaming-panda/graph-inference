40a8fdd4e10d879ba2b58114a9b2bf02f62ccaad
keroro824 <bettychen824@gmail.com>
2024-01-22 22:58:35 +0000

    7b

--- benchmark_graph_performance.sh
+++ benchmark_graph_performance.sh
@@ -1,31 +1,31 @@
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 1 >> benchmark/1.3B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 2 >> benchmark/1.3B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 4 >> benchmark/1.3B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 8 >> benchmark/1.3B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 16 >> benchmark/1.3B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 32 >> benchmark/1.3B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 1 >> benchmark/1.3B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 2 >> benchmark/1.3B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 4 >> benchmark/1.3B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 8 >> benchmark/1.3B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 16 >> benchmark/1.3B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-1.3B --M 256 --P 128  --D 32 >> benchmark/1.3B_benchmark.log
 
 
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 1 >> benchmark/2.7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 2 >> benchmark/2.7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 4 >> benchmark/2.7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 8 >> benchmark/2.7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 16 >> benchmark/2.7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 32 >> benchmark/2.7B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 1 >> benchmark/2.7B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 2 >> benchmark/2.7B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 4 >> benchmark/2.7B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 8 >> benchmark/2.7B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 16 >> benchmark/2.7B_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model princeton-nlp/Sheared-LLaMA-2.7B --M 256 --P 128  --D 32 >> benchmark/2.7B_benchmark.log
 
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 1 >> benchmark/68m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 2 >> benchmark/68m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 4 >> benchmark/68m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 8 >> benchmark/68m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 16 >> benchmark/68m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 32 >> benchmark/68m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 1 >> benchmark/68m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 2 >> benchmark/68m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 4 >> benchmark/68m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 8 >> benchmark/68m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 16 >> benchmark/68m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-68m --M 256 --P 128  --D 32 >> benchmark/68m_benchmark.log
 
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 1 >> benchmark/160m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 2 >> benchmark/160m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 4 >> benchmark/160m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 8 >> benchmark/160m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 16 >> benchmark/160m_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 32 >> benchmark/160m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 1 >> benchmark/160m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 2 >> benchmark/160m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 4 >> benchmark/160m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 8 >> benchmark/160m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 16 >> benchmark/160m_benchmark.log
+#CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 32 >> benchmark/160m_benchmark.log
 
 CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 1 >> benchmark/7B_benchmark.log
 CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 2 >> benchmark/7B_benchmark.log
