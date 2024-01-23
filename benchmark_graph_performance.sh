eb378fb722d54bbc8619b04a4a319d5da0629082
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-01-22 19:13:14 -0500

    Update benchmark_graph_performance.sh

--- benchmark_graph_performance.sh
+++ benchmark_graph_performance.sh
@@ -27,19 +27,19 @@
 #CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 16 >> benchmark/160m_benchmark.log
 #CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model JackFram/llama-160m --M 256 --P 128  --D 32 >> benchmark/160m_benchmark.log
 
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 1 >> benchmark/7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 2 >> benchmark/7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 4 >> benchmark/7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 8 >> benchmark/7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 16 >> benchmark/7B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 32 >> benchmark/7B_benchmark.log
+# CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 1 >> benchmark/7B_benchmark.log
+# CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 2 >> benchmark/7B_benchmark.log
+# CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 4 >> benchmark/7B_benchmark.log
+# CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 8 >> benchmark/7B_benchmark.log
+# CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 16 >> benchmark/7B_benchmark.log
+# CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model meta-llama/Llama-2-7b-hf --M 256 --P 128  --D 32 >> benchmark/7B_benchmark.log
 
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model /fsx-storygen/beidic/zhuoming --M 256 --P 128  --D 1 >> benchmark/33B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model /fsx-storygen/beidic/zhuoming --M 256 --P 128  --D 2 >> benchmark/33B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model /fsx-storygen/beidic/zhuoming --M 256 --P 128  --D 4 >> benchmark/33B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model /fsx-storygen/beidic/zhuoming --M 256 --P 128  --D 8 >> benchmark/33B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model /fsx-storygen/beidic/zhuoming --M 256 --P 128  --D 16 >> benchmark/33B_benchmark.log
-CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model /fsx-storygen/beidic/zhuoming --M 256 --P 128  --D 32 >> benchmark/33B_benchmark.log
+CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model lmsys/vicuna-33b-v1.3 --M 256 --P 128  --D 1 >> benchmark/33B_benchmark.log
+CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model lmsys/vicuna-33b-v1.3 --M 256 --P 128  --D 2 >> benchmark/33B_benchmark.log
+CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model lmsys/vicuna-33b-v1.3 --M 256 --P 128  --D 4 >> benchmark/33B_benchmark.log
+CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model lmsys/vicuna-33b-v1.3 --M 256 --P 128  --D 8 >> benchmark/33B_benchmark.log
+CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model lmsys/vicuna-33b-v1.3 --M 256 --P 128  --D 16 >> benchmark/33B_benchmark.log
+CUDA_VISIBLE_DEVICES=0 python bencmark_graph_inference.py --model lmsys/vicuna-33b-v1.3 --M 256 --P 128  --D 32 >> benchmark/33B_benchmark.log
 
 
 
