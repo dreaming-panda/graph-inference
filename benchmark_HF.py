f14668589190d6f1d923ebb61a842f873ee524c5
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-04-15 15:52:56 -0400

    Update benchmark_HF.py

--- benchmark_HF.py
+++ benchmark_HF.py
@@ -4,8 +4,8 @@ import argparse
 import time
 from torch.profiler import profile, record_function, ProfilerActivity
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default="meta-llama/Llama-2-7b-hf",help='model')
-parser.add_argument('--T', type=int, default=1000, help='repeat times')
+parser.add_argument('--model', type=str, default="meta-llama/Llama-2-70b-hf",help='model')
+parser.add_argument('--T', type=int, default=200, help='repeat times')
 parser.add_argument('--B', type=int, default=1, help='batch size')
 parser.add_argument('--P', type=int, default=128, help='prefix length')
 
@@ -16,7 +16,7 @@ args = parser.parse_args()
 print(args)
 
 #target_model = LlamaForCausalLM_Attn.from_pretrained(args.target, torch_dtype=torch.float16, device_map="auto")
-draft_model = LlamaForCausalLM.from_pretrained(args.model, torch_dtype=torch.float16).cuda()
+draft_model = LlamaForCausalLM.from_pretrained(args.model, torch_dtype=torch.float16)
 
 
 # draft_model = deepspeed.init_inference(draft_model,  
@@ -25,16 +25,16 @@ T = args.T
 B = args.B
 P = args.P
 LEN = [1, 32, 64, 128, 192, 256]
-prefix = torch.randint(low=3, high=30000, size=(B, P)).cuda()
+prefix = torch.randint(low=3, high=30000, size=(B, P))
 past_key_values = draft_model(input_ids = prefix, use_cache=True).past_key_values
 
 PERFORMANCE = []
 with torch.no_grad():
     for l in LEN:
 
-        sentence = torch.randint(low=3, high=30000, size=(B,  l)).cuda()
+        sentence = torch.randint(low=3, high=30000, size=(B,  l))
         total_time = 0.0
-        for _ in range(100):
+        for _ in range(20):
             output = draft_model(input_ids = sentence, use_cache=True, past_key_values=past_key_values)
         torch.cuda.synchronize()
         t1 = time.time()
